9f2ace58f9cf76b7aa9cb3f0e6bc4ed0ec05c227
matheusbach <math.bach.com@gmail.com>
2022-02-16 11:47:04 -0300

    Use xwi88/bbolt instead boltdb/bolt

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -3,17 +3,17 @@ package storage
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/boltdb/bolt"
+	"github.com/xwi88/bbolt"
 	"log"
 )
 
-func InitialiseDB(path string) (*bolt.DB, error) {
-	db, err := bolt.Open(path, 0600, nil)
+func InitialiseDB(path string) (*bbolt.DB, error) {
+	db, err := bbolt.Open(path, 0600, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	err = db.Update(func(tx *bolt.Tx) error {
+	err = db.Update(func(tx *bbolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists([]byte("peers"))
 		if err != nil {
 			return fmt.Errorf("DB: Could not create peers bucket: %v", err)
@@ -32,7 +32,7 @@ func InitialiseDB(path string) (*bolt.DB, error) {
 	return db, nil
 }
 
-func CachePeerToDB(db *bolt.DB, peer string) error {
+func CachePeerToDB(db *bbolt.DB, peer string) error {
 	entry := peer
 	entryBytes, err := json.Marshal(entry)
 
@@ -40,7 +40,7 @@ func CachePeerToDB(db *bolt.DB, peer string) error {
 		return err
 	}
 
-	err = db.Update(func(tx *bolt.Tx) error {
+	err = db.Update(func(tx *bbolt.Tx) error {
 		err := tx.Bucket([]byte("peers")).Put([]byte("peer"), (entryBytes))
 		if err != nil {
 			return err
@@ -55,8 +55,8 @@ func CachePeerToDB(db *bolt.DB, peer string) error {
 	return err
 }
 
-func LoadPeersFromDB(db *bolt.DB) error {
-	err := db.View(func(tx *bolt.Tx) error {
+func LoadPeersFromDB(db *bbolt.DB) error {
+	err := db.View(func(tx *bbolt.Tx) error {
 		peers := tx.Bucket([]byte("peers")).Get([]byte("peer"))
 		log.Println("Peers:\n", string(peers))
 		return nil
