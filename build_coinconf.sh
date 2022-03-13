e840e23620da4ad967e6e9500c5c014db2e46486
matheusbach <math.bach.com@gmail.com>
2022-03-13 14:34:32 -0300

    add new platforms and remove unsuported

--- build_coinconf.sh
+++ build_coinconf.sh
@@ -5,12 +5,15 @@ cd 'cmd/coinconf'
 go get -d -v ./...
 go install -v ./...
 
-package_name="coinconf"
+package_name="coinconf-truenodes"
 
-platforms=( "aix/ppc64" "android/386" "android/amd64" "android/arm" "android/arm64" "darwin/amd64" "darwin/arm64" "dragonfly/amd64" "freebsd/386" "freebsd/amd64" "freebsd/arm" "illumos/amd64" "ios/arm64" "js/wasm" "linux/386" "linux/amd64" "linux/arm" "linux/arm64" "linux/ppc64" "linux/ppc64le" "linux/mips" "linux/mipsle" "linux/mips64" "linux/mips64le" "linux/riscv64" "linux/s390x" "netbsd/386" "netbsd/amd64" "netbsd/arm" "openbsd/386" "openbsd/amd64" "openbsd/arm" "openbsd/arm64" "plan9/386" "plan9/amd64" "plan9/arm" "solaris/amd64" "windows/386" "windows/amd64" )
+platforms=( "aix/ppc64" "android/arm64" "darwin/amd64" "darwin/arm64" "dragonfly/amd64" "freebsd/386" "freebsd/amd64" "freebsd/arm" "freebsd/arm64" "illumos/amd64" "linux/386" "linux/amd64" "linux/arm" "linux/arm64" "linux/mips" "linux/mips64" "linux/mips64le" "linux/mipsle" "linux/ppc64" "linux/ppc64le" "linux/riscv64" "linux/s390x" "netbsd/386" "netbsd/amd64" "netbsd/arm" "netbsd/arm64" "openbsd/386" "openbsd/amd64" "openbsd/arm" "openbsd/arm64" "openbsd/mips64" "solaris/amd64" "windows/386" "windows/amd64" "windows/arm" "windows/arm64" )
+unsuported_platforms_for_now=( "android/386" "android/amd64" "android/arm" "ios/amd64" "ios/arm64" "js/wasm" "plan9/386" "plan9/amd64" "plan9/arm" )
 
 for platform in "${platforms[@]}"
 do
+    echo -e "Building for\u001b[32m $platform\u001b[0m"
+
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
@@ -19,5 +22,5 @@ do
         output_name+='.exe'
     fi
 
-    env CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH GOARM=7 go build -a -tags netgo -ldflags '-w' -o $output_name .
+    env CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH GOARM=7 go build -a -tags netgo -ldflags '-w -s' -o $output_name .
 done
